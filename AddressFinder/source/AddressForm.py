f6713b2b3741d9cfc0c404f19425e89e4a2bbe95
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2025-09-08 08:55:14 -0600

    Add design and style to the interface for searching address

--- AddressFinder/source/AddressForm.py
+++ AddressFinder/source/AddressForm.py
@@ -15,6 +15,7 @@ class AddressFormClass(FlaskForm):
         render_kw = {
             'placeholder': 'Ej: 58048',
             'pattern': '[0-9]{5}',
+            'size': '30',
         },
         id = 'PostalCode'
     )
@@ -22,30 +23,42 @@ class AddressFormClass(FlaskForm):
     Location = StringField(
         'Colonia/Asentamiento:',
         validators = [Optional()],
-        render_kw = {'placeholder': 'Ej: Colonia Manuel Villalongín'},
+        render_kw = {
+            'placeholder': 'Ej: Colonia Manuel Villalongín',
+            'size': '30',
+        },
         id = 'Location',
     )
 
     City = StringField(
         'Ciudad:',
         validators = [Optional()],
-        render_kw = {'placeholder': 'Ej: Morelia'},
+        render_kw = {
+            'placeholder': 'Ej: Morelia',
+            'size': '30',
+        },
         id = 'City',
     )
 
     District = StringField(
         'Municipio:',
         validators = [Optional()],
-        render_kw = {'placeholder': 'Ej: Morelia'},
+        render_kw = {
+            'placeholder': 'Ej: Morelia',
+            'size': '30',
+        },
         id = 'District',
     )
 
     State = StringField(
         'Estado:',
         validators = [Optional()],
-        render_kw = {'placeholder': 'Ej: Michoacán'},
+        render_kw = {
+            'placeholder': 'Ej: Michoacán',
+            'size': '30',
+        },
         id = 'State',
     )
 
     AutocompleteButton = SubmitField('Autocompletar')
-    CleanButton = SubmitField('Limpiar Resultados')
+    CleanButton = SubmitField('Limpiar Resultado')
